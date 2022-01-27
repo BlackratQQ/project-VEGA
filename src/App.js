60ea78bef1873bd68fc88349569f6729e4988019
BlackratQQ <vojtech.kochta@gmail.com>
2022-01-27 17:43:12 +0100

    First Grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+//logo header
+//nav content
+//footer
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header">Header</div>
+      <div className="content">Content</div>
+      <div className="nav">Nav</div>
+      <div className="sidebar">Side</div>
+      <div className="footer">Footer</div>
     </div>
   );
 }
